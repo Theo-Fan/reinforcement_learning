cb34e43f991615ce4bb7e44fd69dedebea18113a
Theo Fan <ffex@Theo-MacBook-Pro.local>
2025-03-21 13:10:51 +0800

    merge

--- preknowledge/hydra/example1/my_app.py
+++ preknowledge/hydra/example1/my_app.py
@@ -4,6 +4,7 @@ import hydra
 
 @hydra.main(version_base=None, config_path=".", config_name="config")
 def my_app(cfg: DictConfig) -> None:
+    print(cfg.db.user)
     print(OmegaConf.to_yaml(cfg))
 
 if __name__ == "__main__":
