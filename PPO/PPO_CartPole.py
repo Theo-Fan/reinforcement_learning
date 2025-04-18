60fe60a040676bea87b737c333b7acdcb4902910
TheoFan <theo@Theos-Mac-mini.local>
2025-04-18 11:30:36 +0800

    update ppo

--- PPO/PPO_CartPole.py
+++ PPO/PPO_CartPole.py
@@ -6,6 +6,9 @@ import sys
 import torch.nn as nn
 from torch.optim import Adam
 from torch.distributions import Categorical
+import torch.nn.functional as F
+import collections
+import random
 
 
 class Actor(nn.Module):
@@ -22,9 +25,8 @@ class Actor(nn.Module):
 
     def forward(self, x):
         t = self.model(x)
-        dist = Categorical(t)
         # entropy = dist.entropy()
-        return dist
+        return t
 
 
 class Critic(nn.Module):
@@ -40,36 +42,6 @@ class Critic(nn.Module):
 
     def forward(self, x):
         return self.model(x)
-
-
-# this buffer is similar to the replay buffer,
-# It used for calc advantage and update actor/critic nets
-class PPOmemory:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.buffer = collections.deque(maxlen=capacity)
-
-    def push(self, state, action, reward, next_state, done, prob):
-        # TODO implement push method
-        self.buffer.append((state, action, reward, next_state, done, prob))
-
-    def sample(self, batch_size):
-        transitions = random.sample(self.buffer, batch_size)
-        state, action, reward, next_state, done = zip(*transitions)
-
-        batch_data = {
-            'state': state,
-            'action': action,
-            'reward': reward,
-            'next_state': next_state,
-            'done': done,
-            'probs': probs,
-        }
-
-        return batch_data
-
-    def size(self):
-        return len(self.buffer)
 
 
 class Agent:
@@ -93,29 +65,67 @@ class Agent:
         self.actor_optimizer = Adam(self.actor.parameters(), lr=self.lr)
         self.critic_optimizer = Adam(self.critic.parameters(), lr=self.lr)
 
-        self.memory = PPOmemory(self.memory_capacity)
-
     def take_action(self, state):
         state_tensor = torch.tensor(state, dtype=torch.float32).view(1, -1)
-        dist = self.actor(state_tensor)
+        dist = Categorical(self.actor(state_tensor))
         dist_prob = dist.probs
         action = dist.sample()
         return action.item(), dist_prob
 
-    def learn(self):
+    def calc_GAE(self, TD_error):
+        advantage_lst = []
+        advantage = 0.0
+        # print(len(TD_error))
+        for delta in TD_error.flip(0):
+            advantage = self.gamma * self.gae_lmbda * advantage + delta
+            advantage_lst.append(advantage)
+
+        advantage_lst.reverse()
+        return torch.tensor(advantage_lst, dtype=torch.float32)
+
+    def learn(self, data):
         # TODO implement learn method
         # 1. calculate advantage
         # 2. update actor and critic networks
-        # 3. clear memory
-        pass
+
+        states = torch.tensor(data['states'], dtype=torch.float32)
+        actions = torch.tensor(data['actions'], dtype=torch.int64).view(-1, 1)
+        rewards = torch.tensor(data['rewards'], dtype=torch.float32).view(-1, 1)
+        next_states = torch.tensor(data['next_states'], dtype=torch.float32)
+        dones = torch.tensor(data['dones'], dtype=torch.float32).view(-1, 1)
+
+        TD_target = rewards + self.gamma * self.critic(next_states) * (1 - dones)
+        TD_error = TD_target - self.critic(states)
+
+        # print(TD_error)
+
+        advantage = self.calc_GAE(TD_error)
+
+        old_log_probs = torch.log(self.actor(states).gather(1, actions)).detach()
+
+        # TODO implement Importance Sampling for PPO
+        for _ in range(10):
+            log_porbs = torch.log(self.actor(states).gather(1, actions))
+            ratio = torch.exp(log_porbs - old_log_probs)  # TODO importance sampling ratio
+            surr1 = ratio * advantage
+            surr2 = torch.clamp(ratio, 1 - self.clip, 1 + self.clip) * advantage  # clipped surrogate
+
+            actor_loss = torch.mean(-torch.min(surr1, surr2))
+            critic_loss = torch.mean(F.mse_loss(self.critic(states), TD_target.detach()))
+
+            self.actor_optimizer.zero_grad()
+            self.critic_optimizer.zero_grad()
+            actor_loss.backward()
+            critic_loss.backward()
+            self.actor_optimizer.step()
+            self.critic_optimizer.step()
 
 
 def main():
     env_name = 'CartPole-v1'
     env = gym.make(env_name)
 
-    num_episodes = 200
-    max_steps = 500
+    num_episodes = 500
 
     state_dim = env.observation_space.shape[0]
     action_dim = int(findall(r"\d+\.?\d*", str(env.action_space))[0])  # 获取动作维度
@@ -126,22 +136,28 @@ def main():
 
     for episode in range(1, num_episodes + 1):
         state = env.reset()[0]
+
+        transition_dict = {'states': [], 'actions': [], 'next_states': [], 'rewards': [], 'dones': []}
+
         episode_reward = 0
 
-        for step in range(1, max_steps + 1):
+        done = False
+        while not done:
             action, probs = ppo_agent.take_action(state)  # TODO
 
             next_state, reward, terminated, truncated, _ = env.step(action)
             done = terminated or truncated
+
+            transition_dict['states'].append(state)
+            transition_dict['actions'].append(action)
+            transition_dict['next_states'].append(next_state)
+            transition_dict['rewards'].append(reward)
+            transition_dict['dones'].append(done)
+
             episode_reward += reward
-
-            ppo_agent.memory.push(state, action, reward, next_state, done, probs)  # TODO
-
-            if done:
-                break
             state = next_state
 
-        ppo_agent.learn()
+        ppo_agent.learn(transition_dict)
         if episode % 10 == 0:
             print(f"回合：{episode}/{num_episodes}，奖励：{episode_reward:.2f}")
     print('完成训练！')
